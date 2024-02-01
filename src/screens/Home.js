0cb08cc90dda75b7bee02d9c7098be8fcd4f346e
Murari Yadav <murari.yadav@thingmzing.com>
2024-02-01 11:16:55 +0530

    MERN project - initial commits modified:   package-lock.json modified:   package.json modified:   public/index.html modified:   src/App.js new file:   src/components/Footer.js new file:   src/components/NavBar.js new file:   src/screens/Home.js

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar.js'
+import Footer from '../components/Footer.js'
+
+export default function Home() {
+    return (
+        <div>
+            <NavBar />
+            <div>body</div>
+            <Footer />
+        </div>
+    )
+}
