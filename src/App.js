85da96e603addf427c9fa16dfeea8a94c286b0ef
Murari Yadav <murari.yadav@thingmzing.com>
2024-02-03 15:43:09 +0530

    bootstrap 5setup 	modified:   package.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './screens/Home';
 import Login from './screens/Login';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
